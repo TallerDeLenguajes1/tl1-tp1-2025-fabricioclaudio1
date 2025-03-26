d1987f997e3a3d8d46b0e5fe4a8008b3780ba383
Fabri <fabricioclaudio1@gmail.com>
2025-03-26 01:34:26 -0300

    e)Punteros, direccion de memoria, tamaño de memoria

--- tp1_1.c
+++ tp1_1.c
@@ -4,6 +4,16 @@ int main(){
 
     printf("Hola mundo\n");
 
+    int num = 2;
+
+    int  *pint = &num;
+
+    printf("Contenido del puntero %d\n", *pint);
+    printf("Dirección de memoria almacenada por el puntero: %p\n", (void *)pint);
+    printf("Dirección de memoria de la variable: %p\n", (void *)&num);
+    printf("Dirección de memoria del puntero: %p\n", (void *)&pint);
+
+    printf("Tamanho de memoria de num: %zu\n", sizeof(num));
 
     return 0;
 }
